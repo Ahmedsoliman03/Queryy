255d3ef60cc660595158ee0d924fb110ca6963be
Ahmedsoliman03 <ahmedmohammedali20003@gmail.com>
2025-10-30 15:31:37 +0300

    add filteration

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,46 @@
-import UsePosts from "../hooks/usePosts"
+import { useState } from "react";
+import UsePosts from "../hooks/usePosts";
 
-const Home =()=>{
-    const {data , isLoading , error} = UsePosts()
-    
-    return(
-       <>
-       
-       {data?.map((p)=>{
-       return (
-    <div key={p.id}>
-      <p>{p.title}</p>
-    </div>
-  )
-       })}
-       </>
-    )
-}
+const Home = () => {
+  const [idVal, setIdVal] = useState("all");
+  const { data, isLoading, error } = UsePosts(idVal);
 
-export default Home 
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message || "Something went wrong"}</p>;
+console.log(data);
+
+  return (
+    <>
+      <select
+        onChange={(e) => setIdVal(e.target.value)}
+        value={idVal}
+        className="border p-1 rounded"
+      >
+        <option value="all">All</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+
+      {/* Check if data exists and handle both array/object responses */}
+      {data ? (
+        Array.isArray(data) ? (
+          data.map((p) => (
+            <div key={p.id} className="mt-2">
+            <p>{p.id}{" "}{p.title}</p>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <div>
+            <p>{data.id}{" "}{data.title}</p>
+          </div>
+        )
+      ) : (
+        <p>No data found.</p>
+      )}
+    </>
+  );
+};
+
+export default Home;
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,12 +1,21 @@
 import axios from "axios"
 import { useQuery } from "@tanstack/react-query"
-const fetchData =async () =>{
-    const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+
+const UsePosts = (idVal) =>{
+    console.log(idVal);
+    
+    const fetchData =async () =>{
+        if(idVal !== "all"){
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${idVal}`)
     return res.data
+        }
+        else{
+                const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+    return res.data
+        }
 }
-const UsePosts = () =>{
     const query = useQuery({
-        queryKey: ["posts"],
+        queryKey: ["posts" , {idVal}],
         queryFn: fetchData,
         staleTime: 1000 * 10 // caching for 10s
     })
